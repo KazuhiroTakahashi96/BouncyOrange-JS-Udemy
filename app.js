5bc5fc6fa9bd9b3144f7a5b230367a70e2f67e00
Kazu Takahashi <num96kazu19@icloud.com>
2022-01-23 17:22:20 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const canvasEl = document.querySelector("canvas");
+const canvasContext = canvasEl.getContext("2d");
+
+const FPS = 60;
+let radius = 50;
+let xP, yP;
+let xV, yV;
+
+xP = canvasEl.width / 2;
+yP = canvasEl.height / 2;
+
+xV = Math.floor(Math.random() * 201 + 99) / FPS;
+yV = Math.floor(Math.random() * 201 + 99) / FPS;
+
+// coin toss situation
+if (Math.floor(Math.random() * 2) === 0) {
+  xV = -xV;
+}
+if (Math.floor(Math.random() * 2) === 0) {
+  yV = -yV;
+}
+
+// ===================================== the game loop
+function runGame() {
+  // ======================moving the ball from its current position
+  xP += xV;
+  yP += yV;
+
+  // ========================clearing the canvas
+  canvasContext.clearRect(0, 0, canvasEl.width, canvasEl.height);
+
+  // ========================collision detection
+  // bottom boundry
+  if (yV > 0 && yP >= canvasEl.height - radius) {
+    yV = -yV;
+  }
+  // right boundry
+  if (xV > 0 && xP > canvasEl.width - radius) {
+    xV = -xV;
+  }
+  // top boundry
+  if (yV < 0 && yP <= radius) {
+    yV = -yV;
+  }
+  // left boundry
+  if (xV < 0 && xP <= radius) {
+    xV = -xV;
+  }
+
+  // drawing the ball
+  canvasContext.beginPath();
+  canvasContext.fillStyle = "orange";
+  //   canvasContext.strokeStyle = "orange";
+  canvasContext.arc(xP, yP, radius, 0, Math.PI * 2);
+  canvasContext.fill();
+  //   canvasContext.stroke();
+}
+
+// set interval
+setInterval(runGame, 1000 / FPS);
